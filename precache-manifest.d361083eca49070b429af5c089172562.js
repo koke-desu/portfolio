b68ec50d8fe0c25ee159792669276b3edabe78fe
koke-desu <gbpvp25645@yahoo.co.jp>
2020-09-21 19:11:16 +0900

    Updates

---
+++ precache-manifest.d361083eca49070b429af5c089172562.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ece66f221c6803124ce5ba45464464",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c60a2d9fd5367bc2ff6c",
+    "url": "/portfolio/static/css/main.6a1ad6a7.chunk.css"
+  },
+  {
+    "revision": "b82748e6e5ae2c727aae",
+    "url": "/portfolio/static/js/2.89407bce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.89407bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60a2d9fd5367bc2ff6c",
+    "url": "/portfolio/static/js/main.bde59cb8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "119142d488e186d68f620e905db33009",
+    "url": "/portfolio/static/media/desk_and_pc2.119142d4.jpg"
+  },
+  {
+    "revision": "665f57a38cab882896107f01a5fc8ede",
+    "url": "/portfolio/static/media/github.665f57a3.jpg"
+  },
+  {
+    "revision": "0d1a7bd1d80f0bcdc9422206546b16d4",
+    "url": "/portfolio/static/media/portfolio_sc.0d1a7bd1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.d39a943e29489efefa29c843417c69b4.js"
+  "/portfolio/precache-manifest.d361083eca49070b429af5c089172562.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
